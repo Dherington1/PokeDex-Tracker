3440bb235cbd0a24aeefa0a4daac9b760bb27a93
Dherington1 <dherington1@comcast.net>
2024-01-02 17:46:10 -0800

    changed package.json stuff to hopefully fix deployed site

--- server/index.js
+++ server/index.js
@@ -44,12 +44,12 @@ app.use('/api/v1/pokedex', pokedexRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    // });
+}
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
 
 async function connectToMongo() {
     try {
